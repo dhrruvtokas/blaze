a6bda481befeb169120df41911b51a60b0669402
Dhrruv Tokas <89195647+dhrruvtokas@users.noreply.github.com>
2021-10-28 10:52:19 +1100

    Add files via upload
    Remaining resources were uploaded

---
+++ Form1.Designer.vb
@@ -0,0 +1,383 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()>
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()>
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()>
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.TextBox1 = New System.Windows.Forms.TextBox()
+        Me.WebBrowser1 = New System.Windows.Forms.WebBrowser()
+        Me.Panel1 = New System.Windows.Forms.Panel()
+        Me.Button8 = New System.Windows.Forms.Button()
+        Me.Button7 = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Button2 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.ColorDialog1 = New System.Windows.Forms.ColorDialog()
+        Me.Panel2 = New System.Windows.Forms.Panel()
+        Me.FacebookMini = New System.Windows.Forms.Label()
+        Me.GooglePlusMini = New System.Windows.Forms.Label()
+        Me.TwitterMini = New System.Windows.Forms.Label()
+        Me.YouTubeMini = New System.Windows.Forms.Label()
+        Me.QuverWeb = New System.Windows.Forms.Label()
+        Me.Facebook = New System.Windows.Forms.Label()
+        Me.Twitter = New System.Windows.Forms.Label()
+        Me.YouTube = New System.Windows.Forms.Label()
+        Me.GooglePlus = New System.Windows.Forms.Label()
+        Me.QuverLabel2 = New System.Windows.Forms.Label()
+        Me.QuverLabel1 = New System.Windows.Forms.Label()
+        Me.NotifyIcon1 = New System.Windows.Forms.NotifyIcon(Me.components)
+        Me.Panel1.SuspendLayout()
+        Me.Panel2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'TextBox1
+        '
+        Me.TextBox1.BackColor = System.Drawing.Color.White
+        Me.TextBox1.Location = New System.Drawing.Point(93, 12)
+        Me.TextBox1.Name = "TextBox1"
+        Me.TextBox1.Size = New System.Drawing.Size(397, 20)
+        Me.TextBox1.TabIndex = 6
+        Me.TextBox1.Tag = "What's Next"
+        '
+        'WebBrowser1
+        '
+        Me.WebBrowser1.Anchor = CType((((System.Windows.Forms.AnchorStyles.Top Or System.Windows.Forms.AnchorStyles.Bottom) _
+            Or System.Windows.Forms.AnchorStyles.Left) _
+            Or System.Windows.Forms.AnchorStyles.Right), System.Windows.Forms.AnchorStyles)
+        Me.WebBrowser1.Location = New System.Drawing.Point(0, 48)
+        Me.WebBrowser1.MinimumSize = New System.Drawing.Size(20, 20)
+        Me.WebBrowser1.Name = "WebBrowser1"
+        Me.WebBrowser1.Size = New System.Drawing.Size(774, 500)
+        Me.WebBrowser1.TabIndex = 7
+        Me.WebBrowser1.Url = New System.Uri(";oik", System.UriKind.Relative)
+        '
+        'Panel1
+        '
+        Me.Panel1.AutoSize = True
+        Me.Panel1.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.Panel1.Controls.Add(Me.Button8)
+        Me.Panel1.Controls.Add(Me.Button7)
+        Me.Panel1.Controls.Add(Me.Button1)
+        Me.Panel1.Controls.Add(Me.TextBox1)
+        Me.Panel1.Controls.Add(Me.Button2)
+        Me.Panel1.Controls.Add(Me.Button3)
+        Me.Panel1.Controls.Add(Me.Button6)
+        Me.Panel1.Controls.Add(Me.Button4)
+        Me.Panel1.Controls.Add(Me.Button5)
+        Me.Panel1.Location = New System.Drawing.Point(0, 0)
+        Me.Panel1.Name = "Panel1"
+        Me.Panel1.Size = New System.Drawing.Size(1368, 53)
+        Me.Panel1.TabIndex = 9
+        '
+        'Button8
+        '
+        Me.Button8.AutoSize = True
+        Me.Button8.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button8.Image = Global.Browser.My.Resources.Resources.Graphicloads_100_Flat_Heart_favourite
+        Me.Button8.Location = New System.Drawing.Point(722, 3)
+        Me.Button8.Name = "Button8"
+        Me.Button8.Size = New System.Drawing.Size(40, 40)
+        Me.Button8.TabIndex = 9
+        Me.Button8.UseVisualStyleBackColor = True
+        '
+        'Button7
+        '
+        Me.Button7.AutoSize = True
+        Me.Button7.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button7.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button7.ForeColor = System.Drawing.SystemColors.ControlText
+        Me.Button7.Image = Global.Browser.My.Resources.Resources.Graphicloads_Colorful_Long_Shadow_Printer
+        Me.Button7.Location = New System.Drawing.Point(677, 4)
+        Me.Button7.Name = "Button7"
+        Me.Button7.Size = New System.Drawing.Size(40, 40)
+        Me.Button7.TabIndex = 8
+        Me.Button7.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.AutoSize = True
+        Me.Button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button1.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button1.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button1.Image = Global.Browser.My.Resources.Resources.Graphicloads_100_Flat_2_Arrow_back
+        Me.Button1.Location = New System.Drawing.Point(3, 3)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(41, 40)
+        Me.Button1.TabIndex = 0
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Button2
+        '
+        Me.Button2.AutoSize = True
+        Me.Button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button2.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button2.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button2.Image = Global.Browser.My.Resources.Resources.Graphicloads_100_Flat_2_Arrow_next_3
+        Me.Button2.Location = New System.Drawing.Point(50, 3)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(40, 40)
+        Me.Button2.TabIndex = 1
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.AutoSize = True
+        Me.Button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button3.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button3.ForeColor = System.Drawing.Color.Black
+        Me.Button3.Image = Global.Browser.My.Resources.Resources.Graphicloads_100_Flat_Home
+        Me.Button3.Location = New System.Drawing.Point(633, 3)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(40, 40)
+        Me.Button3.TabIndex = 2
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Button6
+        '
+        Me.Button6.AutoSize = True
+        Me.Button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button6.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button6.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button6.Image = Global.Browser.My.Resources.Resources.Graphicloads_100_Flat_2_Arrow_refresh_3
+        Me.Button6.Location = New System.Drawing.Point(539, 2)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(40, 40)
+        Me.Button6.TabIndex = 5
+        Me.Button6.Text = " "
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.AutoSize = True
+        Me.Button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button4.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button4.ForeColor = System.Drawing.SystemColors.ActiveCaptionText
+        Me.Button4.Image = Global.Browser.My.Resources.Resources.Graphicloads_Colorful_Long_Shadow_Search
+        Me.Button4.Location = New System.Drawing.Point(496, 3)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(40, 40)
+        Me.Button4.TabIndex = 3
+        Me.Button4.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.AutoSize = True
+        Me.Button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.Button5.Font = New System.Drawing.Font("Rosewood Std Regular", 9.749999!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Button5.ForeColor = System.Drawing.Color.Black
+        Me.Button5.Image = Global.Browser.My.Resources.Resources.Osullivanluke_Orb_Os_X_OSX
+        Me.Button5.Location = New System.Drawing.Point(591, 3)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(40, 40)
+        Me.Button5.TabIndex = 4
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Panel2
+        '
+        Me.Panel2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Panel2.Controls.Add(Me.FacebookMini)
+        Me.Panel2.Controls.Add(Me.GooglePlusMini)
+        Me.Panel2.Controls.Add(Me.TwitterMini)
+        Me.Panel2.Controls.Add(Me.YouTubeMini)
+        Me.Panel2.Location = New System.Drawing.Point(290, 356)
+        Me.Panel2.Name = "Panel2"
+        Me.Panel2.Size = New System.Drawing.Size(200, 100)
+        Me.Panel2.TabIndex = 16
+        '
+        'FacebookMini
+        '
+        Me.FacebookMini.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.FacebookMini.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.FacebookMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.FacebookMini.Image = Global.Browser.My.Resources.Resources.Facebook
+        Me.FacebookMini.Location = New System.Drawing.Point(160, 15)
+        Me.FacebookMini.Name = "FacebookMini"
+        Me.FacebookMini.Size = New System.Drawing.Size(32, 41)
+        Me.FacebookMini.TabIndex = 15
+        '
+        'GooglePlusMini
+        '
+        Me.GooglePlusMini.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.GooglePlusMini.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GooglePlusMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.GooglePlusMini.Image = Global.Browser.My.Resources.Resources.Google_
+        Me.GooglePlusMini.Location = New System.Drawing.Point(15, 15)
+        Me.GooglePlusMini.Name = "GooglePlusMini"
+        Me.GooglePlusMini.Size = New System.Drawing.Size(40, 41)
+        Me.GooglePlusMini.TabIndex = 12
+        '
+        'TwitterMini
+        '
+        Me.TwitterMini.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.TwitterMini.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.TwitterMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.TwitterMini.Image = Global.Browser.My.Resources.Resources.Sicons_Basic_Round_Social_Twitter
+        Me.TwitterMini.Location = New System.Drawing.Point(113, 15)
+        Me.TwitterMini.Name = "TwitterMini"
+        Me.TwitterMini.Size = New System.Drawing.Size(41, 41)
+        Me.TwitterMini.TabIndex = 14
+        '
+        'YouTubeMini
+        '
+        Me.YouTubeMini.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.YouTubeMini.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.YouTubeMini.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.YouTubeMini.Image = Global.Browser.My.Resources.Resources.Martz90_Circle_Youtube
+        Me.YouTubeMini.Location = New System.Drawing.Point(61, 15)
+        Me.YouTubeMini.Name = "YouTubeMini"
+        Me.YouTubeMini.Size = New System.Drawing.Size(46, 41)
+        Me.YouTubeMini.TabIndex = 13
+        '
+        'QuverWeb
+        '
+        Me.QuverWeb.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.QuverWeb.Image = Global.Browser.My.Resources.Resources.Web_Browsers_RockMelt_Metro_icon
+        Me.QuverWeb.Location = New System.Drawing.Point(-3, 300)
+        Me.QuverWeb.Name = "QuverWeb"
+        Me.QuverWeb.Size = New System.Drawing.Size(87, 44)
+        Me.QuverWeb.TabIndex = 21
+        '
+        'Facebook
+        '
+        Me.Facebook.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Facebook.Image = Global.Browser.My.Resources.Resources.facebook_icon
+        Me.Facebook.Location = New System.Drawing.Point(-3, 245)
+        Me.Facebook.Name = "Facebook"
+        Me.Facebook.Size = New System.Drawing.Size(87, 44)
+        Me.Facebook.TabIndex = 20
+        '
+        'Twitter
+        '
+        Me.Twitter.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.Twitter.Image = Global.Browser.My.Resources.Resources.Web_Twitter_alt_2_Metro_icon
+        Me.Twitter.Location = New System.Drawing.Point(-3, 190)
+        Me.Twitter.Name = "Twitter"
+        Me.Twitter.Size = New System.Drawing.Size(87, 44)
+        Me.Twitter.TabIndex = 19
+        '
+        'YouTube
+        '
+        Me.YouTube.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.YouTube.Image = Global.Browser.My.Resources.Resources.youtube_icon
+        Me.YouTube.Location = New System.Drawing.Point(-3, 135)
+        Me.YouTube.Name = "YouTube"
+        Me.YouTube.Size = New System.Drawing.Size(87, 44)
+        Me.YouTube.TabIndex = 18
+        '
+        'GooglePlus
+        '
+        Me.GooglePlus.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.GooglePlus.Image = Global.Browser.My.Resources.Resources.Web_Google_plus_alt_Metro_icon1
+        Me.GooglePlus.Location = New System.Drawing.Point(-3, 81)
+        Me.GooglePlus.Name = "GooglePlus"
+        Me.GooglePlus.Size = New System.Drawing.Size(87, 44)
+        Me.GooglePlus.TabIndex = 17
+        '
+        'QuverLabel2
+        '
+        Me.QuverLabel2.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.QuverLabel2.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.QuverLabel2.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.QuverLabel2.Image = Global.Browser.My.Resources.Resources.QuverLogo
+        Me.QuverLabel2.Location = New System.Drawing.Point(286, 87)
+        Me.QuverLabel2.Name = "QuverLabel2"
+        Me.QuverLabel2.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.QuverLabel2.Size = New System.Drawing.Size(209, 213)
+        Me.QuverLabel2.TabIndex = 11
+        Me.QuverLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'QuverLabel1
+        '
+        Me.QuverLabel1.Anchor = System.Windows.Forms.AnchorStyles.None
+        Me.QuverLabel1.BackColor = System.Drawing.SystemColors.ButtonHighlight
+        Me.QuverLabel1.FlatStyle = System.Windows.Forms.FlatStyle.Flat
+        Me.QuverLabel1.Image = Global.Browser.My.Resources.Resources.QuverLogo
+        Me.QuverLabel1.Location = New System.Drawing.Point(286, 162)
+        Me.QuverLabel1.Name = "QuverLabel1"
+        Me.QuverLabel1.RightToLeft = System.Windows.Forms.RightToLeft.No
+        Me.QuverLabel1.Size = New System.Drawing.Size(209, 213)
+        Me.QuverLabel1.TabIndex = 10
+        Me.QuverLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter
+        '
+        'NotifyIcon1
+        '
+        Me.NotifyIcon1.Text = "NotifyIcon1"
+        Me.NotifyIcon1.Visible = True
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.BackColor = System.Drawing.SystemColors.HotTrack
+        Me.ClientSize = New System.Drawing.Size(774, 542)
+        Me.Controls.Add(Me.QuverWeb)
+        Me.Controls.Add(Me.Facebook)
+        Me.Controls.Add(Me.Twitter)
+        Me.Controls.Add(Me.YouTube)
+        Me.Controls.Add(Me.GooglePlus)
+        Me.Controls.Add(Me.Panel2)
+        Me.Controls.Add(Me.QuverLabel2)
+        Me.Controls.Add(Me.QuverLabel1)
+        Me.Controls.Add(Me.Panel1)
+        Me.Controls.Add(Me.WebBrowser1)
+        Me.Icon = CType(resources.GetObject("$this.Icon"), System.Drawing.Icon)
+        Me.Name = "Form1"
+        Me.Text = "Blaze"
+        Me.Panel1.ResumeLayout(False)
+        Me.Panel1.PerformLayout()
+        Me.Panel2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Button2 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button6 As Button
+    Friend WithEvents TextBox1 As TextBox
+    Friend WithEvents WebBrowser1 As WebBrowser
+    Friend WithEvents Button7 As Button
+    Friend WithEvents Panel1 As Panel
+    Friend WithEvents ColorDialog1 As ColorDialog
+    Friend WithEvents Button8 As Button
+    Friend WithEvents QuverLabel1 As Label
+    Friend WithEvents QuverLabel2 As Label
+    Friend WithEvents GooglePlusMini As Label
+    Friend WithEvents YouTubeMini As Label
+    Friend WithEvents TwitterMini As Label
+    Friend WithEvents FacebookMini As Label
+    Friend WithEvents Panel2 As Panel
+    Friend WithEvents GooglePlus As Label
+    Friend WithEvents YouTube As Label
+    Friend WithEvents Twitter As Label
+    Friend WithEvents Facebook As Label
+    Friend WithEvents QuverWeb As Label
+    Friend WithEvents NotifyIcon1 As NotifyIcon
+End Class
---
+++ Form1.vb
@@ -0,0 +1,174 @@
+﻿Public Class Form1
+    Private Sub Button6_Click(sender As Object, e As EventArgs) Handles Button6.Click
+        WebBrowser1.Refresh()
+    End Sub
+
+    Private Sub Button4_Click(sender As Object, e As EventArgs) Handles Button4.Click
+        WebBrowser1.Navigate(TextBox1.Text)
+        QuverLabel1.Hide()
+        QuverLabel2.Hide()
+        Panel2.Hide()
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub Button5_Click(sender As Object, e As EventArgs) Handles Button5.Click
+        WebBrowser1.Stop()
+    End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        QuverLabel1.Hide()
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+        QuverLabel2.Show()
+        Panel2.Show()
+        GooglePlusMini.Show()
+        YouTubeMini.Show()
+        TwitterMini.Show()
+        FacebookMini.Show()
+        WebBrowser1.GoHome()
+    End Sub
+
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        WebBrowser1.GoBack()
+        QuverLabel2.Hide()
+        Panel2.Hide()
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub Button2_Click(sender As Object, e As EventArgs) Handles Button2.Click
+        WebBrowser1.GoForward()
+        QuverLabel2.Hide()
+        Panel2.Hide()
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        QuverLabel2.Hide()
+        Panel2.Hide()
+    End Sub
+
+    Private Sub TextBox1_TextChanged(sender As Object, e As EventArgs) Handles TextBox1.TextChanged
+    End Sub
+
+    Private Sub Button7_Click(sender As Object, e As EventArgs) Handles Button7.Click
+        WebBrowser1.Print()
+    End Sub
+
+    Private Sub Button8_Click(sender As Object, e As EventArgs) Handles Button8.Click
+        If ColorDialog1.ShowDialog = DialogResult.OK Then
+            Panel1.BackColor = ColorDialog1.Color
+        End If
+    End Sub
+
+    Private Sub QuverLabel2_Click(sender As Object, e As EventArgs) Handles QuverLabel2.Click
+
+    End Sub
+
+    Private Sub Label1_Click(sender As Object, e As EventArgs) Handles GooglePlus.Click
+        WebBrowser1.Navigate("https://plus.google.com/100106782027663570840/posts")
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+    End Sub
+
+    Private Sub YouTube_Click(sender As Object, e As EventArgs) Handles YouTube.Click
+        WebBrowser1.Navigate("https://www.youtube.com/channel/UC_C0nuvJ-TFWCq4X0zG4Cuw/featured")
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+    End Sub
+
+    Private Sub Twitter_Click(sender As Object, e As EventArgs) Handles Twitter.Click
+        WebBrowser1.Navigate("https://twitter.com/QuverForums")
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+    End Sub
+
+    Private Sub Facebook_Click(sender As Object, e As EventArgs) Handles Facebook.Click
+        WebBrowser1.Navigate("https://www.facebook.com/Quver-1044704895573929/?ref=hl")
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+    End Sub
+
+    Private Sub QuverWeb_Click(sender As Object, e As EventArgs) Handles QuverWeb.Click
+        WebBrowser1.Navigate("https://www.quver.webs.com")
+        GooglePlus.Hide()
+        YouTube.Hide()
+        Twitter.Hide()
+        Facebook.Hide()
+        QuverWeb.Hide()
+    End Sub
+
+    Private Sub Label2_Click(sender As Object, e As EventArgs) Handles GooglePlusMini.Click
+        WebBrowser1.Navigate("https://plus.google.com/100106782027663570840/posts")
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub YouTubeMini_Click(sender As Object, e As EventArgs) Handles YouTubeMini.Click
+        WebBrowser1.Navigate("https://www.youtube.com/channel/UC_C0nuvJ-TFWCq4X0zG4Cuw/featured")
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub TwitterMini_Click(sender As Object, e As EventArgs) Handles TwitterMini.Click
+        WebBrowser1.Navigate("https://twitter.com/QuverForums")
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub FacebookMini_Click(sender As Object, e As EventArgs) Handles FacebookMini.Click
+        WebBrowser1.Navigate("https://www.facebook.com/Quver-1044704895573929/?ref=hl")
+        GooglePlusMini.Hide()
+        YouTubeMini.Hide()
+        TwitterMini.Hide()
+        FacebookMini.Hide()
+    End Sub
+
+    Private Sub TextBox2_TextChanged(sender As Object, e As EventArgs)
+    End Sub
+
+    Private Sub TextBox2_TextChanged_1(sender As Object, e As EventArgs) 
+    End Sub
+
+    Private Sub Button9_Click(sender As Object, e As EventArgs) 
+        TextBox1.SelectAll()
+        TextBox1.Copy()
+        TextBox2.Paste()
+        TextBox2.SelectAll()
+        TextBox2.b
+    End Sub
+End Class
